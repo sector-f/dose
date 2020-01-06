7d143b746080d35a3e44f4470313321e08971937
Adam <sector-f@users.noreply.github.com>
2020-01-05 22:19:38 -0500

    Added code to generate bytes for the wire for me

--- header.go
+++ header.go
@@ -1,6 +1,7 @@
 package dose
 
 import (
+	"bytes"
 	"encoding/binary"
 	"encoding/json"
 	"errors"
@@ -21,6 +22,48 @@ const (
 	ErrorResponseMessage      MessageType = 9
 	DownloadResponseMessage   MessageType = 10
 )
+
+func MakeBody(r interface{}) ([]byte, error) {
+	var messageType MessageType
+
+	switch r.(type) {
+	case AddRequest:
+		messageType = AddRequestMessage
+	case CancelRequest:
+		messageType = CancelRequestMessage
+	case RemoveRequest:
+		messageType = RemoveRequestMessage
+	case InfoRequest:
+		messageType = InfoRequestMessage
+	case ServerInfoRequest:
+		messageType = ServerInfoRequestMessage
+	case AddedResponse:
+		messageType = AddedResponseMessage
+	case CanceledResponse:
+		messageType = CanceledResponseMessage
+	case InfoResponse:
+		messageType = InfoResponseMessage
+	case ServerInfoResponse:
+		messageType = ServerInfoResponseMessage
+	case ErrorResponse:
+		messageType = ErrorResponseMessage
+	case DownloadResponse:
+		messageType = DownloadResponseMessage
+	default:
+		return nil, errors.New("Invalid type")
+	}
+
+	data, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+
+	header := DoseHeader{messageType, uint16(len(data))}
+	headerBytes := header.ToBytes()
+	response := append(headerBytes[:], data...)
+
+	return response, nil
+}
 
 func ParseBody(messageType MessageType, body []byte) (interface{}, error) {
 	switch messageType {
@@ -50,6 +93,19 @@ func ParseBody(messageType MessageType, body []byte) (interface{}, error) {
 type DoseHeader struct {
 	MessageType MessageType
 	Length      uint16
+}
+
+func (h DoseHeader) ToBytes() [4]byte {
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.BigEndian, h)
+	bytes := buf.Bytes()
+
+	return [4]byte{
+		bytes[0],
+		bytes[1],
+		bytes[2],
+		bytes[3],
+	}
 }
 
 func ParseHeader(header [4]byte) DoseHeader {
